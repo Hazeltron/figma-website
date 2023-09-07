210f5a3186079444e71a9f53b58bfa21ac4cc5c6
Hazeltron <paulhazelton.work@gmail.com>
2023-09-06 19:28:00 -0600

    Added HTML structure to pages

--- page-2.php
+++ page-2.php
@@ -1,3 +1,61 @@
-<?php include('modules/header/index.php') ?>
+<section class="header wavy-head">
+	<inner-column>
+		
+		<?php include('modules/header/index.php') ?>
 
-<?php include('modules/footer/index.php') ?>
+	</inner-column>
+</section>
+
+<section class="hero wavy-hero">
+	<inner-column>
+
+		<?php include('modules/hero/index.php'); ?>
+
+	</inner-column>
+</section>
+
+<section class="video wavy-video">
+	<inner-column>
+		
+	<?php include('modules/video/index.php')?>
+
+	</inner-column>
+</section>
+
+<section class="grid-list wavy-grid-list">
+	<inner-column>
+		
+	<?php include('modules/grid-list/index.php')?>
+	
+	</inner-column>
+</section>
+
+<section class="clients wavy-clients">
+	<inner-column>
+<?php include('modules/client/template.php')?>
+	
+	</inner-column>
+</section>
+
+<section class="traditional-cta wavy-cta">
+	<inner-column>
+<?php include('modules/traditional-cta/template.php')?>
+	
+	</inner-column>
+</section>
+
+<section class="questions wavy-questions">
+	<inner-column>
+<?php include('modules/questions/template.php')?>
+	
+	</inner-column>
+</section>
+
+
+</main>
+<section class="footer wavy-footer">
+	<inner-column>
+
+		<?php include('modules/footer/index.php') ?>
+	</inner-column>
+</section>
--- page-3.php
+++ page-3.php
@@ -1,10 +1,61 @@
-<?php include('modules/header/index.php') ?>
-
-
-
-
-<section class="footer">
+<section class="header color-head">
 	<inner-column>
+		
+		<?php include('modules/header/index.php') ?>
+
+	</inner-column>
+</section>
+
+<section class="hero color-hero">
+	<inner-column>
+
+		<?php include('modules/hero/index.php'); ?>
+
+	</inner-column>
+</section>
+
+<section class="video color-video">
+	<inner-column>
+		
+	<?php include('modules/video/index.php')?>
+
+	</inner-column>
+</section>
+
+<section class="grid-list color-grid-list">
+	<inner-column>
+		
+	<?php include('modules/grid-list/index.php')?>
+	
+	</inner-column>
+</section>
+
+<section class="clients color-clients">
+	<inner-column>
+<?php include('modules/client/template.php')?>
+	
+	</inner-column>
+</section>
+
+<section class="traditional-cta color-cta">
+	<inner-column>
+<?php include('modules/traditional-cta/template.php')?>
+	
+	</inner-column>
+</section>
+
+<section class="questions color-questions">
+	<inner-column>
+<?php include('modules/questions/template.php')?>
+	
+	</inner-column>
+</section>
+
+
+</main>
+<section class="footer color-footer">
+	<inner-column>
+
 		<?php include('modules/footer/index.php') ?>
 	</inner-column>
 </section>
