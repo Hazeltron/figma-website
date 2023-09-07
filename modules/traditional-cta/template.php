19d36c579d0606bad1bfaae68b3704cbae2126b4
Hazeltron <paulhazelton.work@gmail.com>
2023-09-06 19:28:34 -0600

    Added content to cta section

--- modules/traditional-cta/template.php
+++ modules/traditional-cta/template.php
@@ -1,7 +1,19 @@
 <simple-cta>
 	
+	<text-content>
 	<p class="calm-voice">Lorem ipsum dolor, sit amet.</p>
 	<h2 class="attention-voice">Lorem ipsum, dolor, sit amet consectetur adipisicing elit. Harum, amet!</h2>
-	<a class="calm-voice" href="#">get free quotes</a>
+	</text-content>
+
+	<button-control>
+		<a class="calm-voice quotes" href="#">get free quotes</a>
+	<a class="calm-voice optional-link" href="#">free signup</a>	
+	<input type="text" placeholder="Email Adress">
+	</button-control>
+	
+	<picture>
+		<img src="assets/square.png" alt="">
+	</picture>
+	
 	
 </simple-cta>
