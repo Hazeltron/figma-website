d0972269e79b083baacb4762dfe2824aef544fd6
Hazeltron <paulhazelton.work@gmail.com>
2023-09-06 19:29:08 -0600

    Added content and split the css between three page hero

--- modules/hero/index.php
+++ modules/hero/index.php
@@ -1,13 +1,15 @@
 
 <hero>
 
-	<h1 class="loud-voice">Words</h1>
+	<h1 class="loud-voice">Lorem ipsum dolor sit amet.</h1>
 	<p class="calm-voice">Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Ratione architecto, labore fuga placeat cum atque fugiat corrupti magni nam, delectus.</p>
 
 	<button-box>
 
-		<a href="#">get started</a>
-		<a href="#">start free trial</a>
+		<a class="get-started" href="#">get started</a>
+		<a class="trial" href="#">start free trial</a>
+		<a class="signup" href="">signup</a>
+		<input type="text" placeholder="Email Adress">
 		
 	</button-box>
 
