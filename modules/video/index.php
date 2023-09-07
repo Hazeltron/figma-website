40f5bd1b92faefa2b1857fc853d5558833473bca
Hazeltron <paulhazelton.work@gmail.com>
2023-09-06 20:44:57 -0600

    Updated video loop and video json

--- modules/video/index.php
+++ modules/video/index.php
@@ -5,33 +5,39 @@
 		<img src="assets/square.png" alt="">
 	</picture>
 
-	<p class="calm-voice">Lorem, ipsum dolor sit amet consectetur, adipisicing elit. </p>
+	<p class="calm-voice optional-p">Lorem, ipsum dolor sit amet consectetur, adipisicing elit. </p>
 
 	<h2 class="attention-voice">watch this</h2>
 
-	<p class="calm-voice">Lorem ipsum dolor sit amet, consectetur adipisicing, elit.</p>
+	<p class="calm-voice default-p">Lorem ipsum dolor sit amet, consectetur adipisicing, elit.</p>
 
 	<iframe class="video-iframe" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="Rick Astley - Never Gonna Give You Up (Official Music Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
 	<ul>
-
 	<list-area>
+
 	<?php  
-	$json = file_get_contents('data/video-data.json');
+	$json = file_get_contents('data/video.json');
 	$options = json_decode($json, true);
 	foreach($options as $option) { ?>
 
-	
 		<li>
-			<h3 class="strong-voice"></h3>
-			<p class="calm-voice"></p>
+			<h3 class="strong-voice"><?=$option['header']?></h3>
+			<p class="calm-voice"><?=$option['copy']?></p>
 		</li>
+
 	<?php } ?>
+
 	</list-area>
 	</ul>
 
-	<a class="learn-link" href="#">learn more</a>
-	<a class="see-link" href="">see all</a>
+	<button-control>
+
+		<a class="learn-link" href="#">learn more</a>
+		<a class="see-link" href="">see all</a>
+
+	</button-control>
+	
 
 	<bottom-text-content>
 
