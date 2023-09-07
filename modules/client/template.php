3f0c47fd73e6eec82a17b6c996efa44b40483bce
Hazeltron <paulhazelton.work@gmail.com>
2023-09-06 23:24:05 -0600

    Added content and CSS across three client pages

--- modules/client/template.php
+++ modules/client/template.php
@@ -14,15 +14,30 @@
 		$clients = json_decode($json, true); 
 		foreach ($clients as $client) {?>
 
+			<picture class="large-img">
+				<img src="assets/square.png" alt="">
+			</picture>
+
 			<li>
 				<picture class="client-icon">
 					<img src="<?=$client['icon']?>" alt="">
 				</picture>
-				<h3 class="calm-voice"><?=$client['copy']?></h3>
-				<picture class="client-photo">
-					<img src="<?=$client['photo']?>" alt="">
-				</picture>
-				<p class="calm-voice"><?=$client['name']?></p>
+				<h3 class="strong-voice"><?=$client['copy']?></h3>
+
+				<client-info>
+
+					<picture class="client-photo">
+						<img src="<?=$client['photo']?>" alt="">
+					</picture>
+					<div class="client-info-text">
+						<p class="calm-voice"><?=$client['name']?></p>
+						<p class="calm-voice job"><?=$client['job']?></p>
+						<p class="calm-voice description"><?=$client['description']?></p>
+					</div>
+					
+
+				</client-info>
+				
 			</li>
 			
 
