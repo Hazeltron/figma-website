6dd58402c7cfdf27a9df4714df863950cf82d225
Hazeltron <paulhazelton.work@gmail.com>
2023-09-10 23:08:03 -0600

    Updated footer so it functions

--- modules/footer/index.php
+++ modules/footer/index.php
@@ -4,54 +4,111 @@
 		<nav class='site-menu'>
 
 			<!-- change to json loop -->
-			<ul>
-				<li>
-					<h3 class="strong-voice">product</h3>
-					<a href="#">Lorem ipsum dolor sit.</a>
-					<a href="#">Lorem ipsum dolor sit.</a>
-					<a href="#">Lorem ipsum dolor sit.</a>
-					<a href="#">Lorem ipsum dolor sit.</a>
-					<a href="#">Lorem ipsum dolor sit.</a>
-					<a href="#">Lorem ipsum dolor sit.</a>
-				</li>
-				
-				
-			</ul>
 
-			<ul>
-				<li>
-					<h3 class="strong-voice">information</h3>
-					<a href="#">Lorem ipsum dolor sit.</a>
-					<a href="#">Lorem ipsum dolor sit.</a>
-					<a href="#">Lorem ipsum dolor sit.</a>
-				</li>
-			</ul>
-				
+			<div class="footer-grid">
 
-			<ul>
-				<li>
-					<h3 class="strong-voice">company</h3>
-					<a href="#">Lorem ipsum dolor sit.</a>
-					<a href="#">Lorem ipsum dolor sit.</a>
-					<a href="#">Lorem ipsum dolor sit.</a>
-				</li>
-			</ul>
+				<ul>
+					<li>
 
-			<ul>
-				<li>
-					<h3 class="strong-voice">subscribe</h3>
+						<h3 class="strong-voice">product</h3>
+						<a href="#">Lorem</a>
+						<a href="#">Lorem</a>
+						<a href="#">Lorem</a>
+						<a href="#">Lorem</a>
+						<a href="#">Lorem</a>
+						<a href="#">Lorem</a>
 
-					<div class="email-container">
-						<input class="email-input" type="text" placeholder="Email Adress">
-						<a class="button-color" href="">icon</a>
-					</div>
-
-					<p>Gravida sed justo, justo, id est et. Amet tristique convallis sed porttitor nullam eu ut. Duis et odio aliquam bibendum. Metus et lectus id viverra fringilla magna morbi. 
-					</p>
+					</li>
 					
-				</li>
+					
+				</ul>
 
-			</ul>
+				<ul>
+					<li>
+
+						<h3 class="strong-voice">information</h3>
+						<a href="#">Lorem ipsum</a>
+						<a href="#">Lorem ipsum</a>
+						<a href="#">Lorem ipsum</a>
+
+					</li>
+				</ul>
+					
+
+				<ul>
+					<li>
+
+						<h3 class="strong-voice">company</h3>
+						<a href="#">Lorem ipsum dolor</a>
+						<a href="#">Lorem ipsum dolor</a>
+						<a href="#">Lorem ipsum dolor</a>
+
+					</li>
+				</ul>
+
+				<ul>
+					<li>
+
+						<h3 class="strong-voice">subscribe</h3>
+
+						<div class="email-container">
+							<input class="email-input" type="text" placeholder="Email Adress">
+							<a class="button-color" href="">icon</a>
+						</div>
+
+						<p>Gravida sed justo, justo, id est et. Amet tristique convallis sed porttitor nullam eu ut. Duis et odio aliquam bibendum. Metus et lectus id viverra fringilla magna morbi. 
+						</p>
+						
+					</li>
+
+				</ul>
+				
+			</div>
+			
+
+			<div class="footer-bottom">
+
+				<ul class="logo-footer">
+					<li>
+
+						<picture>
+							<img src="assets/square.png" alt="">
+						</picture>
+
+					</li>
+					
+				</ul>
+
+				<ul class="legal">
+					<li>
+
+						<a href="#">terms</a>
+						<a href="#">privacy</a>
+						<a href="#">cookies</a>
+
+					</li>
+				</ul>
+
+				<ul class="social-media">
+					<li>
+
+						<picture>
+							<img src="assets/square.png" alt="">
+						</picture>
+
+						<picture>
+							<img src="assets/square.png" alt="">
+						</picture>
+
+						<picture>
+							<img src="assets/square.png" alt="">
+						</picture>
+
+					</li>
+				</ul>
+				
+			</div>
+			
 				
 			
 		</nav>
